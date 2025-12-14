7159ff47d395641db7955812bd082f1da6b7e9d0
dingfeng <fengDing0321@gmail.com>
2025-12-14 17:18:30 +0800

    update deployment

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -3,6 +3,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.BASE_URL || '/',
   resolve: {
     alias: {
       '@schema/core': path.resolve(__dirname, '../core/src/index.ts'),
